d9afac316312ba813028009780fe7e7d402ee77f
Danielogbebor <lifeofkerry@gmail.com>
2023-04-22 20:33:39 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:my_todo_app/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'To Do App',
+        theme: ThemeData(primarySwatch: Colors.brown),
+        home: const HomePage());
+  }
+}
